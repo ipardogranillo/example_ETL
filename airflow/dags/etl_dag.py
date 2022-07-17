e2b74f6ae2e1eac65b8c0502be7f3969feb9bc5a
Isaac Pardo Granillo <ipardogranillo@gmail.com>
2022-07-17 16:18:56 -0500

    dumping parquet table from weather api data

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -1,5 +1,5 @@
+import os
 from datetime import datetime
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.exceptions import AirflowException
@@ -7,17 +7,21 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.utils.trigger_rule import TriggerRule
-
-
-
 from scripts import current
 from scripts import api
 from scripts import municipalities
 
+
 DEFAULT_ARGS = {
-    "owner": "ipardogranillo",
-    "retries": 1 ,
+    "owner": "airflow",
+    "retries": 0,
 }
+OP_KWARGS = {
+    "url": "https://smn.conagua.gob.mx/webservices/index.php?method=1",
+    "run_time": int(datetime.now().strftime("%Y%m%d%H")),
+    "api_dump_path":os.path.abspath("dags/scripts/dw/weather_api/") + "/w_api.parquet",
+}
+
 
 @task(trigger_rule=TriggerRule.ONE_FAILED, retries=1)
 def watcher():
@@ -35,28 +39,27 @@ with DAG(
 ) as dag:
 
     start = DummyOperator(task_id="start")
-
     api_data = PythonOperator(
         task_id="get_weather",
         python_callable=api.weather_api,
+        op_kwargs=OP_KWARGS,
     )
-
     mun_data = PythonOperator(
         task_id="append_muns",
         python_callable=municipalities.append_muns,
+        op_kwargs=OP_KWARGS,
     )
-
     curr_data = PythonOperator(
         task_id="enrich_current",
         python_callable=current.enrich_current,
+        op_kwargs=OP_KWARGS,
     )
-
     end = BashOperator(
         task_id="end",
         bash_command="echo 'finished execution'",
         trigger_rule=TriggerRule.ALL_DONE,
     )
 
-    start >> api_data >> mun_data >> curr_data >> end
 
+    start >> api_data >> mun_data >> curr_data >> end
     list(dag.tasks) >> watcher()
--- airflow/dags/scripts/api.py
+++ airflow/dags/scripts/api.py
@@ -1,11 +1,61 @@
 import requests
-import codecs
 import gzip
+import json
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+import os
+
+def get_api_response(url):
+    print("creating requests session...")
+    print("making request...")
+    response = requests.get(url, verify=False)
+    print(response.request)
+    print(response)
+    if not response:
+        print("ERROR: no response received")
+        return None
+    else:
+        print("decompressing gzip file...")
+        decomp_gz = gzip.decompress(response.content)
+        if not decomp_gz:
+            print("ERROR: decompression unsuccessful")
+            return None
+        else:
+            print("parsing json...")
+            json_res = json.loads(decomp_gz)
+            if not json_res:
+                return False
+            else:
+                return json_res
 
 
-def weather_api():
-    print("getting api data...")
+def dump_api_response(json_res, run_time, dump_path):
+    if type(json_res) == None:
+        print("no dataframe to create")
+        return False
+    else:
+        n_rows = len(json_res)
+        if n_rows < 1:
+            print("ERROR: no rows to process")
+            return False
+        else:
+            run_time_col = [run_time] * n_rows
+            print("creating dataframe with run time index")
+            df = pd.DataFrame(json_res, index=run_time_col)
+            print(f"loaded {len(df)} rows and {len(df.columns)} columns")
+            print("dataframe to parquet...")
+            table = pa.Table.from_pandas(df)
+            print(f"dumping parquet to {dump_path}...")
+            
+            
+            pq.write_table(table, dump_path)
 
-    print("dumping api data...")
 
-    return True
+def weather_api(**kwargs):
+    api_reponse = get_api_response(kwargs["url"])
+    return dump_api_response(api_reponse, kwargs["run_time"], kwargs["api_dump_path"])
+
+print(os.path.abspath("."))
+print(os.listdir("/opt/"))
