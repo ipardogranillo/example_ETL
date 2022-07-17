d3d23619fb7b44ce3327e1515f94692aa15a6d3b
Isaac Pardo Granillo <ipardogranillo@gmail.com>
2022-07-17 18:52:43 -0500

    changed to dataset and dump path validation

--- airflow/dags/scripts/api.py
+++ airflow/dags/scripts/api.py
@@ -1,9 +1,11 @@
+import os
 import requests
 import gzip
 import json
 import pandas as pd
 import pyarrow as pa
-import pyarrow.parquet as pq
+from pyarrow import parquet as pq
+from  pyarrow import dataset as ds
 
 
 def get_api_response(url):
@@ -11,6 +13,8 @@ def get_api_response(url):
     print("making request...")
     response = requests.get(url, verify=False)
     print(response.request)
+    print(response.request.body)
+    print(response.request.headers)
     print(response)
     if not response:
         print("ERROR: no response received")
@@ -48,8 +52,14 @@ def dump_api_response(json_res, run_time, dump_path):
             print("dataframe to parquet...")
             table = pa.Table.from_pandas(df, preserve_index=True)
             print(f"dumping parquet to {dump_path}")
-            pq.write_table(table, dump_path)
-            return True
+            if(os.path.exists(dump_path + "/" + str(run_time) + "/")):
+                print(f"parquet already exists for run time {run_time}")
+                return None
+            else:
+                ds.write_dataset(table, dump_path, format="parquet", partitioning=ds.partitioning(
+                    pa.schema([("run_time", pa.int32())])
+                ))
+                return True
 
 
 def weather_api(**kwargs):
